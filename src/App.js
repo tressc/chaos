9063b71e174c0b84d757c72addea0fabf8290c42
Colin <tresscd@gmail.com>
2018-08-21 18:58:44 -0400

    Add browserrouter and switch to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/home.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
