737cb78d5e50264d169278015bc89c9c085fff62
Colin <tresscd@gmail.com>
2018-08-30 19:23:34 -0400

    Add redux store and root reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/store.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  const preloadedState = {};
+  const store = configureStore(preloadedState);
+
+  ReactDOM.render(<App store={ store } />, root);
+});
 registerServiceWorker();
---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const middleWares = [logger, thunk];
+
+const configureStore = (preloadedState = {}) => {
+  return (
+    createStore( rootReducer, preloadedState, applyMiddleware(...middleWares))
+  );
+};
+
+export default configureStore;
