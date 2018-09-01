a21522d45849ca7bf1f95845e428721f19edffc2
Colin <tresscd@gmail.com>
2018-09-01 19:09:39 -0400

    Add todo to tokens container

--- src/components/tokens/tokens_container.js
+++ src/components/tokens/tokens_container.js
@@ -2,6 +2,9 @@ import { connect } from 'react-redux';
 import Tokens from './tokens.jsx';
 
 const msp = (state) => {
+  // TODO: this should fetch from UI > TOKEN POOL,
+  // which should get set when the difficulty is set.
+  // This provides an object that can be modified.
   const current_difficulty = state.ui.current_difficulty;
   const tokens = state.static.difficulties[current_difficulty];
   const tokensArr = Object.keys(tokens);
